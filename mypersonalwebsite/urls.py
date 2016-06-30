49a05cecc46859920e23764b2eb2b9ca2f8c7278
Phillis Kiragu <kiragu.w.phillis@gmail.com>
2016-06-30 22:58:32 +0300

    add website files

--- mypersonalwebsite/urls.py
+++ mypersonalwebsite/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from core.views import dashboard
+from core import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.dashboard, name='dashboard'),
 ]
---
+++ static/main/main.js
@@ -0,0 +1,3 @@
+/**
+ * Created by phillo on 6/30/16.
+ */
